4979a5a1a3d540a02076bb36df0d70e6f9c9d37f
Abhinav8195 <149519597+Abhinav8195@users.noreply.github.com>
2024-07-02 12:45:40 +0530

    Error handling of no loaction

--- src/components/Weather/Index.jsx
+++ src/components/Weather/Index.jsx
@@ -5,6 +5,7 @@ const Index = () => {
     const [search, setSearch] = useState('');
     const [loading, setLoading] = useState(false);
     const [weather, setWeather] = useState(null);
+    const [error, setError] = useState(false);
 
     async function fetchWeatherData(param) {
         setLoading(true);
@@ -12,15 +13,21 @@ const Index = () => {
             const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${param}&appid=d76abbc8d87f259a795ba722dc401afe`);
             const data = await response.json();
             console.log(data);
-            if (data) {
+            if (response.ok) {
                 setLoading(false);
                 setWeather(data);
+                setError(false);
+            } else {
+                setLoading(false);
+                setError(true);
             }
         } catch (error) {
             setLoading(false);
             console.log(error);
+            setError(true);
         }
     }
+    
 
     async function handleSearch() {
         fetchWeatherData(search);
@@ -54,38 +61,39 @@ const Index = () => {
             <Search search={search} setSearch={setSearch} handleSearch={handleSearch} />
             {
                 loading ? <div className='loading'>loading...</div> :
-                    <div>
-                        <div className='CityName'>
-                            <h2>{weather?.city?.name}, <span>{weather?.city?.country}</span></h2>
-                        </div>
-                        <div className='date'>
-                            <span>{getCurrentDate()}</span>
-                        </div>
-                        <div className='temperature'>
-                            Temperature: {kelvinToCelsius(weather?.list[0]?.main?.temp)}°C
-                        </div>
-                        <div className='description'>
-                            <p>{weather?.list[0]?.weather[0]?.description}</p>
-                            <img
-                                src={`http://openweathermap.org/img/wn/${weather?.list[0]?.weather[0]?.icon}@2x.png`}
-                                alt={weather?.list[0]?.weather[0]?.description}
-                            />
-                        </div>
-                        <div className='info'>
-                            <div className='col'>
-                                <div>
-                                    <p className='wind'>{mpsToKmh(weather?.list[0]?.wind?.speed)} km/h ({mpsToMph(weather?.list[0]?.wind?.speed)} mph)</p>
-                                    <p>Wind Speed</p>
+                    error ? <div className='no-location'>No location found. Please try again.</div> :
+                        <div>
+                            <div className='CityName'>
+                                <h2>{weather?.city?.name}, <span>{weather?.city?.country}</span></h2>
+                            </div>
+                            <div className='date'>
+                                <span>{getCurrentDate()}</span>
+                            </div>
+                            <div className='temperature'>
+                                Temperature: {kelvinToCelsius(weather?.list[0]?.main?.temp)}°C
+                            </div>
+                            <div className='description'>
+                                <p>{weather?.list[0]?.weather[0]?.description}</p>
+                                <img
+                                    src={`http://openweathermap.org/img/wn/${weather?.list[0]?.weather[0]?.icon}@2x.png`}
+                                    alt={weather?.list[0]?.weather[0]?.description}
+                                />
+                            </div>
+                            <div className='info'>
+                                <div className='col'>
+                                    <div>
+                                        <p className='wind'>{mpsToKmh(weather?.list[0]?.wind?.speed)} km/h ({mpsToMph(weather?.list[0]?.wind?.speed)} mph)</p>
+                                        <p>Wind Speed</p>
+                                    </div>
+                                </div>
+                                <div className='col'>
+                                    <div>
+                                        <p className='humidity'>{weather?.list[0]?.main?.humidity}%</p>
+                                        <p>Humidity</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div className='col'>
-                                <div>
-                                    <p className='humidity'>{weather?.list[0]?.main?.humidity}%</p>
-                                    <p>Humidity</p>
-                                </div>
-                            </div>
                         </div>
-                    </div>
             }
         </div>
     );
